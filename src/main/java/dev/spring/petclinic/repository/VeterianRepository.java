10bf2c30f83ec93c2affd4f5c1bfa1b48e0a5ce8
yezanee <rbfrk02@naver.com>
2025-03-10 12:27:05 +0900

    Owner 등록 로직 임시 구현

--- src/main/java/dev/spring/petclinic/repository/VeterianRepository.java
+++ src/main/java/dev/spring/petclinic/repository/VeterianRepository.java
@@ -1,7 +1,7 @@
-//package dev.spring.petclinic.repository;
-//
-//import dev.spring.petclinic.domain.Veterian;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface VeterianRepository extends JpaRepository<Veterian, Long> {
-//}
+package dev.spring.petclinic.repository;
+
+import dev.spring.petclinic.domain.Veterian;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VeterianRepository extends JpaRepository<Veterian, Long> {
+}
