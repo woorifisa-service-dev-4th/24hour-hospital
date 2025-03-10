45aee9641ae59a436c6e4553c51838864f47fc4f
[devbae1101] <[dev.bae1101@gmail.com]>
2025-03-10 17:28:01 +0900

    feat. Owner find 기능구현

--- src/main/java/dev/spring/petclinic/repository/OwnerRepository.java
+++ src/main/java/dev/spring/petclinic/repository/OwnerRepository.java
@@ -1,10 +1,15 @@
 package dev.spring.petclinic.repository;
 
 import dev.spring.petclinic.domain.Owner;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface OwnerRepository extends JpaRepository<Owner,Long> {
     Optional<Owner> findById(Long id);
+
+    Page<Owner> findByLastNameContainingIgnoreCase(String lastName, Pageable pageable);
+
 }
