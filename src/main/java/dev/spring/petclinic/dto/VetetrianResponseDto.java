fa398981dfeaf2659886bc32764a5835d97f2f37
noeyoes <>
2025-03-13 15:19:52 +0900

    Fix: confilct 해결

---
+++ src/main/java/dev/spring/petclinic/dto/VetetrianResponseDto.java
@@ -0,0 +1,28 @@
+package dev.spring.petclinic.dto;
+
+import dev.spring.petclinic.domain.Vets;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class VetetrianResponseDto {
+    private long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String specialist;
+
+    public static VetetrianResponseDto of(Vets vets) {
+        return VetetrianResponseDto.builder()
+                .id(vets.getId())
+                .firstName(vets.getFirstName())
+                .lastName(vets.getLastName())
+                .specialist(vets.getSpecialist())
+                .build();
+    }
+}
